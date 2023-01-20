f923c8f0bab27461548e9f125b1e437c59fd219a
LilXann1 <x.lovvorn@gmail.com>
2023-01-20 15:40:22 -0500

    Revision
    Added an exit message and header

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -3,4 +3,8 @@ from pymongo import MongoClient
 url = “mongodb+srv://admin:admin@cluster0.zkuaeue.mongodb.net/?retryWrites=true&w=majority”
 client = MongoClient(mongodb+srv://admin:admin@cluster0.zkuaeue.mongodb.net/?retryWrites=true&w=majority)
 db = client.pytech
-print(db.list_collection_names)
+print("\n -- Pytech Collection List --")
+print(db.list_collection_names())
+
+# show an exit message
+input("\n\n  End of program, press any key to exit... ")
---
+++ module_6/pytech_update.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+
+# Connecting to MongoDB, MongoDB Cluster, and pytech database
+url = url = 'mongodb+srv://admin:admin@cluster0.zkuaeue.mongodb.net/?retryWrites=true&w=majority'
+client = MongoClient(url)
+db = client.pytech
+
+# Going to students collection
+students = db.students
+
+# find all students in the collection 
+student_list = students.find({})
+
+# display message 
+print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+
+# loop over the collection and output the results 
+for doc in student_list:
+    print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
+
+# update student_id 1007
+result = students.update_one({"student_id": "1007"}, {"$set": {"last_name": "Smith"}})
+
+# find the updated student document 
+thorin = students.find_one({"student_id": "1007"})
+
+# display message
+print("\n  -- DISPLAYING STUDENT DOCUMENT 1007 --")
+
+# output the updated document to the terminal window
+print("  Student ID: " + thorin["student_id"] + "\n  First Name: " + thorin["first_name"] + "\n  Last Name: " + thorin["last_name"] + "\n")
+
+# exit message 
+input("\n\n  End of program, press any key to continue...")
